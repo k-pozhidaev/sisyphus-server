abac20e43ed3a70081e45806792941f4d55a8939
Konstantin Pozhidaev <k.pozhidaev@sap.com>
2019-02-11 19:14:30 +0200

    Files chunk controll.

--- src/main/java/io/pozhidaev/sisyphus/service/LocalStorage.java
+++ src/main/java/io/pozhidaev/sisyphus/service/LocalStorage.java
@@ -4,33 +4,95 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.AsynchronousFileChannel;
 import java.nio.file.*;
+import java.util.Objects;
+import java.util.function.Function;
+
+import static java.nio.file.StandardOpenOption.WRITE;
 
 @Slf4j
 @Service
 public class LocalStorage implements FileStorage {
 
     private Path fileDirectory;
+    private Function<Path, Mono<AsynchronousFileChannel>> channelFunction;
 
     @Autowired
     public void setFileDirectory(Path fileDirectory) {
         this.fileDirectory = fileDirectory;
     }
 
+    @Autowired
+    public void setChannelFunction(Function<Path, Mono<AsynchronousFileChannel>> channelFunction) {
+        this.channelFunction = channelFunction;
+    }
+
     @Override
     public Mono<Integer> putObject(final Long id, final Flux<DataBuffer> parts) {
         return parts
             .map(v -> flushBufferToFile(v, id))
-            .reduce(0, (a, v) -> a + v);
+            .reduce(0, Integer::sum);
     }
 
-    private int flushBufferToFile(final DataBuffer dataBuffer, final Long id){
+    public Mono<Void> createFile(final Long id, final long fileSize) {
+        return Mono.fromRunnable(() -> {
+            try {
+                Files.createFile(Paths.get(Objects.toString(id)));
+
+            } catch (IOException e) {
+                throw new RuntimeException("creation failed");
+            }
+        });
+    }
+
+    public Mono<Integer> writeChunk(final Long id, final Flux<DataBuffer> parts, final long offset, final long size) {
+
+        final Mono<AsynchronousFileChannel> channel = channelFunction.apply(Paths.get(id.toString()));
+
+        final Mono<Integer> writeAndGetWritten = channel
+            .flatMapMany(asynchronousFileChannel -> DataBufferUtils.write(parts, asynchronousFileChannel, offset))
+            .map(dataBuffer -> {
+                final int capacity = dataBuffer.capacity();
+                DataBufferUtils.release(dataBuffer);
+                return capacity;
+            })
+            .doOnComplete(() -> {
+                channel.subscribe(this::closeChannel);
+            })
+            .doOnError(throwable -> channel.subscribe(this::closeChannel))
+            .reduce(Integer::sum);
+
+
+        return parts
+            .map(DataBuffer::capacity)
+            .reduce(Integer::sum)
+            .map(integer -> {
+                if (!Long.valueOf(integer.longValue()).equals(size)) {
+                    throw new RuntimeException("Buffer not equals size");
+                }
+                return integer;
+            })
+            .then(writeAndGetWritten);
+
+    }
+
+    private void closeChannel(final AsynchronousFileChannel asynchronousFileChannel){
+        try {
+            asynchronousFileChannel.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Channel close error");
+        }
+    }
+
+    private int flushBufferToFile(final DataBuffer dataBuffer, final Long id) {
         try {
             final InputStream inputStream = dataBuffer.asInputStream(true);
             int bytesLength = inputStream.available();
@@ -41,9 +103,9 @@ public class LocalStorage implements FileStorage {
             }
 
             Files.write(
-                    file,
-                    IOUtils.toByteArray(inputStream),
-                    StandardOpenOption.APPEND
+                file,
+                IOUtils.toByteArray(inputStream),
+                StandardOpenOption.APPEND
             );
 
             inputStream.close();
--- src/test/java/io/pozhidaev/sisyphus/service/LocalStorageTest.java
+++ src/test/java/io/pozhidaev/sisyphus/service/LocalStorageTest.java
@@ -4,18 +4,24 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.core.io.buffer.DefaultDataBuffer;
 import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Objects;
 
+import static java.nio.file.StandardOpenOption.DELETE_ON_CLOSE;
+import static java.nio.file.StandardOpenOption.WRITE;
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -51,6 +57,46 @@ public class LocalStorageTest {
 
         final Path fileDir = Paths.get("fileDirectoryThatsNotExists");
         localStorage.setFileDirectory(fileDir);
-        localStorage.putObject(1L, body).subscribe(v -> assertEquals(v, Integer.valueOf(3)));
+        localStorage.putObject(1L, body)
+            .subscribe(v -> assertEquals(v, Integer.valueOf(3)));
+    }
+
+    @Test
+    public void writeChunk() throws IOException {
+
+        if (Files.exists(Paths.get("1"))) Files.delete(Paths.get("1"));
+        Files.createFile(Paths.get(Objects.toString(1L))).toAbsolutePath();
+        localStorage.setChannelFunction(path -> Mono.fromSupplier(() -> {
+            try {
+                return AsynchronousFileChannel.open(path, WRITE);
+            } catch (IOException e) {
+                throw new RuntimeException("File open operation fault");
+            }
+        }));
+        localStorage
+            .writeChunk(1L, Flux.just(stringBuffer("foo"), stringBuffer("baz")), 0L, 6L)
+            .subscribe(integer -> assertEquals(integer, Integer.valueOf(3)));
+        localStorage.
+            writeChunk(1L, Flux.just(stringBuffer("bar")), 6L, 3L)
+            .subscribe(integer -> assertEquals(integer, Integer.valueOf(3)));
+
+
+    }
+
+    @Test
+    public void name() {
+
+    }
+
+
+    protected DataBuffer stringBuffer(String value) {
+        return byteBuffer(value.getBytes(StandardCharsets.UTF_8));
+    }
+
+    protected DataBuffer byteBuffer(byte[] value) {
+        final DataBufferFactory bufferFactory = new DefaultDataBufferFactory(true);
+        DataBuffer buffer = bufferFactory.allocateBuffer(value.length);
+        buffer.write(value);
+        return buffer;
     }
 }
