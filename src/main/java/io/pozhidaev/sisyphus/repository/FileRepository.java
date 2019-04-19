575df5062648ca0a217e3e87a37f46144844a0cb
Konstantin Pozhidaev <kos89jed@gmail.com>
2019-04-19 13:53:37 +0300

    added task

--- src/main/java/io/pozhidaev/sisyphus/repository/FileRepository.java
+++ src/main/java/io/pozhidaev/sisyphus/repository/FileRepository.java
@@ -4,7 +4,9 @@ package io.pozhidaev.sisyphus.repository;
 import io.pozhidaev.sisyphus.domain.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+/**
+ * TODO make it abstract
+ */
 public interface FileRepository extends JpaRepository<File, Long> {
 
 
